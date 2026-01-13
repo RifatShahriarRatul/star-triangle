7532f7c5cb530c6ca988c053463db6d3ffe54efc
RifatShahriarRatul <rifat.sh.ratul@gmail.com>
2026-01-14 00:25:50 +0600

    Solid star pyramid

--- star-triangle.c
+++ star-triangle.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,k,n;
+    int i,j,k, n;
     printf("Enter the number of rows for the triangle: ");
     scanf("%d", &n);
     for(i=0 ; i<n; i++)
     {
-        for (j=0; j<=i; j++)
+        for(k=i; k<n ; k++)
+        {
+            printf(" ");
+        }
+        for(j=0; j<=2*i; j++)
         {
             printf("*");
         }
