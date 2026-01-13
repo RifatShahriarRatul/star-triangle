869018511d4b53dce52b1c677db20dda35599c2f
RifatShahriarRatul <rifat.sh.ratul@gmail.com>
2026-01-14 00:37:58 +0600

    Void star pyramid

--- star-triangle.c
+++ star-triangle.c
@@ -4,16 +4,27 @@ int main()
     int i,j,k, n;
     printf("Enter the number of rows for the triangle: ");
     scanf("%d", &n);
-    for(i=0 ; i<n; i++)
-    {
-        for(k=i; k<n ; k++)
+    for(k=0; k<=n ; k++)
         {
             printf(" ");
         }
-        for(j=0; j<=2*i; j++)
+        printf("*\n");
+    for(i=1 ; i<=n; i++)
+    {
+        for(k=i; k<=n ; k++)
+        {
+            printf(" ");
+        }
+        printf("*");
+        for(k=0; k<2*i-1; k++)
+        {
+            printf(" ");
+        }
+        printf("*");
+        printf("\n");
+    }
+    for(j=0; j<=2*n+2 ; j++)
         {
             printf("*");
         }
-        printf("\n");
-    }
 }
